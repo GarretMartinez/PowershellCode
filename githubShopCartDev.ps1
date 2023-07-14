f704a409102f26c5556dcdf9c879c3db2e892b83
Garret Martinez <gtmrz22@gmail.com>
2023-07-13 21:29:25 -0600

    added shopping cart dev

---
+++ githubShopCartDev.ps1
@@ -0,0 +1,17 @@
+﻿$publishUrl="https://github.com/GarretMartinez/PowershellCode/"
+$packageName = "C:\Users\mrkom\OneDrive\Documents\Spring 2023\CIT361 Advanced Scripting\githubShopCartDev.ps1"
+$username="GarretMartinez"
+$password="Pr0gram$22"
+
+$params = @{
+  UseBasicParsing = $true
+  Uri             = $publishUrl
+  Method          = "PUT"
+  InFile          = $packageName
+  Headers         = @{
+    ContentType   = "application/gzip"
+    Authorization = "Basic $([System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("$username`:$password")))" 
+  }
+  Verbose         = $true
+}
+Invoke-WebRequest @params
